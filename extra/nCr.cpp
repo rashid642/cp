b19f02f27c845f0a4eb1b18fafc5ceff00c69883
Mohammad Faiz Alam <111381699+MohammadFaizAlam@users.noreply.github.com>
2022-11-09 12:15:37 +0530

    added ncr

---
+++ extra/nCr.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+class nCr{
+    public:
+    vector<long long> fac;
+    int mod;
+    nCr(int _mod){
+        mod = _mod; 
+        fac.resize(1e6+5, 0);
+        fac[0] = 1;
+        for(int i=1; i<fac.size(); i++){
+            fac[i] = (fac[i-1] * i)%mod;
+        }
+    }
+    long long binaryExponentiation(long long a, long long b){
+        // a^b;
+        if(b == 0) return 1;
+        long long ans = binaryExponentiation(a, b/2);
+        ans = (ans*ans) % mod;
+        if(b%2) ans = (ans*a) % mod;
+        return ans;
+    }
+    long long moduloInverse(long long a){
+        return binaryExponentiation(a, mod - 2);
+    }
+    long long combination(int n, int r){
+        long long ans = fac[n];
+        ans = (ans * moduloInverse(fac[n-r])) % mod;
+        ans = (ans * moduloInverse(fac[r])) % mod;
+        return ans;
+    }
+};
+int main(){
+    // cout << "at start" << endl;
+    nCr n(1e9+7);
+    long long ans = n.combination(100,95);
+    cout << ans << endl;
+    return 0;
+}
---
+++ leetcode-template.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define rep(n, m) for(int i=n; i<m; i++)
+#define repj(n, m) for(int j=n; j<m; j++)
+#define rrep(n, m) for(int i=n; i>=m; i--)
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define sv(v) sort(v.begin(), v.end())
+#define sr(v) sort(v.begin(), v.end()); reverse(v.begin(), v.end());
+#define sa(arr, n) sort(arr, arr+n)
+#define endll "\n\n"
+#define endl "\n"
+#define imax INT_MAX
+#define imin INT_MIN 
+#define sum(v) accumulate(v.begin(), v.end(), 0LL);
+#define index(i, j) cout << "(" << i << "," << j << ") "
+#define index3(i, j, k) cout << "(" << i << "," << j << "," << k << ") "
+#define all(v) v.begin(), v.end()
+#define PI 3.141592653589793
+
+void tv(vector<int> v){
+    for(auto it: v) cout << it << " "; cout << endl;
+}
+///// COPY LEETCODE CLASS /////
+
+
+int main(){
+
+}
