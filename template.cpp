1633f8bb4381116026508145b0be44ae1c13ab30
Rashid Aziz <md.r.a.n.786@gmail.com>
2022-11-14 21:12:42 +0530

    somthing

--- template.cpp
+++ template.cpp
@@ -19,18 +19,6 @@ using namespace std;
 #define index3(i, j, k) cout << "(" << i << "," << j << "," << k << ") "
 #define all(v) v.begin(), v.end()
 #define PI 3.141592653589793
-
-void traverseArray(ll arr[], int n); 
-void traverseVector(vector<ll> v);
-vector<bool> primeNumber(int n);
-void mapTraversal(map<char, int> &mpp);
-vector<int> testCases();
-vector<ll> getFactors(ll n); // O(sqrt(n))
-string StringtestCases();
-ll power(ll a, ll b, ll modu); // a^b
-void makeSet(int parent[], int rank[], int n);
-int findParent(int parent[], int node);
-void unionDSU(int u, int v, int parent[], int rank[]);
 // number of subset index ind belong = (ind+1)(n-ind) with 0 indexing
 
 ll giveSqrt(ll x){
@@ -41,10 +29,42 @@ ll giveSqrt(ll x){
             ans = mid;
             low = mid+1;
         }
-        else
-            high = mid-1;
+        else high = mid-1;
     }
     return ans;
+}
+ll power(ll a, ll b, ll modu){ // a ^ b
+    if(b == 0) return 1LL;
+    ll ans = power(a, b/2, modu);
+    ans = (ans * ans)%modu;
+    if(b%2) ans = (ans*a);
+    return ans % modu;
+}
+vector<ll> getFactors(ll n){
+    vector<ll> v;
+    for(ll i=1; i*i <= n; i++){
+        if(n%i == 0){
+            v.push_back(i);
+            if(i != n/i) v.push_back(n/i);
+        }
+    }
+    return v;
+}
+void tv(vector<ll> v){
+    for(int i=0; i<v.size(); i++) cout << v[i] << " "; cout << endl;
+}
+void tv(vector<int> v){
+    for(int i=0; i<v.size(); i++) cout << v[i] << " "; cout << endl;
+}
+vector<bool> primeNumber(int n){
+    vector<bool> v(n, true);
+    v[0] = false; v[1] = false; v[2] = true;
+    for(int i=2; i<n; i++){
+        if(v[i]){
+            for(int j=i*2; j<n; j = j+i) v[j] = false;
+        }
+    }
+    return v;
 }
 class nCr{
     public:
@@ -106,7 +126,6 @@ class DSU{
     }
 };
 void solve(){
-
 }
 int main() {
     srand(time(NULL));
@@ -121,47 +140,4 @@ int main() {
 }
 
 
-ll power(ll a, ll b, ll modu){
-    // a ^ b
-    if(b == 0){
-        return 1LL;
-    }
-    ll ans = power(a, b/2, modu);
-    ans = (ans * ans)%modu;
-    if(b%2){
-        ans = (ans*a);
-    }
-    return ans % modu;
-}
-vector<ll> getFactors(ll n){
-    vector<ll> v;
-    for(ll i=1; i*i <= n; i++){
-        if(n%i == 0){
-            v.push_back(i);
-            if(i != n/i)
-                v.push_back(n/i);
-        }
-    }
-    return v;
-}
-void traverseVector(vector<ll> v){
-    int n = v.size();
-    for(int i=0; i<n; i++){
-        cout << v[i] << " ";
-    }
-    cout << endl;
-}
-vector<bool> primeNumber(int n){
-    vector<bool> v(n, true);
-    v[0] = false;
-    v[1] = false;
-    v[2] = true;
-    for(int i=2; i<n; i++){
-        if(v[i]){
-            for(int j=i*2; j<n; j = j+i){
-                v[j] = false;
-            }
-        }
-    }
-    return v;
-}
+
