bbec7431615874f5567e6b97e8addfa56617a2be
Md. Rashid Aziz <md.r.a.n.786@gmail.com>
2024-07-13 13:56:23 +0530

    changed

--- c.cpp
+++ c.cpp
@@ -2,21 +2,7 @@
 using namespace std;
 
 void solve(int test){
-    int n;
-    cin >> n;
-    long long v[n]; 
-    set<int> st;   
-    map<int, int> mpp;
-    for(int i=0; i<n; i++) cin >> v[i];
-    for(int i=0; i<n; i++){
-        int newpos = i + v[i%n];
-        mpp[(newpos + n)%n]++;
-    }
-    if(mpp.size() == n){
-        cout << "YES\n";
-    }else{
-        cout << "NO\n";
-    }
+    
 }
 int main() {
     ios::sync_with_stdio(false);
